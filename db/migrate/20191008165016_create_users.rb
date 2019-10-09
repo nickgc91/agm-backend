36f5984904499121e5e723ff052d225787882d49
Nicolas Charlet <nicolas@nicolascharlet.com>
2019-10-09 15:50:14 +0100

    created goal model and controller and added email and acc partner to user model

--- db/migrate/20191008165016_create_users.rb
+++ db/migrate/20191008165016_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+      t.string :email
+      t.integer :accountability_partner
 
       t.timestamps
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.destroy_all
-User.create(username: "Elliot", password: '123456')
-User.create(username: "Harry", password: '123456')
-User.create(username: "Rod", password: '123456')
-User.create(username: "Adnan", password: '123456')
+Goal.destroy_all
+
+User.create(username: "Elliot", password: '123456', email: "elliot@elliot.com", accountability_partner: 4)
+User.create(username: "Harry", password: '123456', email: "Harry@Harry.com", accountability_partner: 1)
+User.create(username: "Rod", password: '123456', email: "Rod@Rod.com", accountability_partner: 2)
+User.create(username: "Adnan", password: '123456', email: "Adnan@Adnan.com", accountability_partner: 3)
+
+Goal.create(title: "4 runs per week", completion_status: "30%", user_id: 2)
+Goal.create(title: "travel to 6 new countries", completion_status: "70%", user_id: 4)
+Goal.create(title: "meditate every day", completion_status: "80%", user_id: 2)
+Goal.create(title: "set monthly budget and stick to it", completion_status: "10%", user_id: 1)
+Goal.create(title: "eat salad for dinner 3 times a week", completion_status: "50%", user_id: 4)
+
+
+
