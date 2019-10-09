4cde5e4071a9547ddecd0a5cbc336bba971104b5
Nicolas Charlet <nicolas@nicolascharlet.com>
2019-10-09 14:43:41 +0100

    user model and controller set up, rendering json on server, seed data created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,41 @@
+class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def signin
+        user = User.find_by(username: params[:username])
+
+        if user and user.authenticate(params[:password])
+            render json: user
+        else
+            render json: { error: 'Username/password combniation invalid!' }, status: 401 
+        end
+    end
+
+
+    def create
+        user = User.new(user_params)
+        
+        if user.save
+            render json: user, status: :created
+        else
+            render json: { user_errors: user.errors.full_messages }, status: :unprocessable_entity
+        end 
+    end
+
+    def show 
+        user = User.find(params[:id])
+        render json: user
+    end 
+
+
+    private 
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+User.create(username: "Elliot", password: '123456')
+User.create(username: "Harry", password: '123456')
+User.create(username: "Rod", password: '123456')
+User.create(username: "Adnan", password: '123456')
