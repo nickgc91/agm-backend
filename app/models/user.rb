89d1d2f03304d27b22baea22d6b1ec6367adff59
Nicolas Charlet <nicolas@nicolascharlet.com>
2019-10-15 12:35:17 +0100

    accountability partner last meeting done and new seed data created and zero goals option evaulated for backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
     has_many :action_items, through: :goals
     has_many :journalings
     has_one :life_status_tracker
+
+    def last_talked
+        self.last_meeting.to_s.slice(0..9)
+    end
 end
